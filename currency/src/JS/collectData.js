344f0eb9fc56647db122ed0b28c86f871ef5903d
MihajloVat <vatolkinmihajlo@gmail.com>
2025-03-20 02:36:57 +0200

    always try to find easier way

--- currency/src/JS/collectData.js
+++ currency/src/JS/collectData.js
@@ -3,21 +3,15 @@ const path = require('path');
 
 async function DataFetch() {
   try {
-    const responses = await Promise.all([
-      fetch(
-        'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json'
-      ),
-      fetch(
-        'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?date=20250318&json'
-      ),
-      fetch(
-        'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?date=20250317&json'
-      ),
-    ]);
-    const data = await Promise.all(responses.map((res) => res.json()));
+    const response = await fetch(
+      'https://bank.gov.ua/NBU_Exchange/exchange_site?start=20230101&end=20231231&valcode=USD&sort=exchangedate&order=desc&json'
+    );
+    const data = response.json();
 
     return data;
   } catch (err) {
     console.log('error while fetching:', err);
   }
 }
+
+DataFetch().then((data) => console.log(data));
