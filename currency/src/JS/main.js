e3bc87c0e9e536d04d9df92b459c31e99735bbd3
MihajloVat <vatolkinmihajlo@gmail.com>
2025-03-24 02:24:07 +0200

    midwork save 4

--- currency/src/JS/main.js
+++ currency/src/JS/main.js
@@ -1,15 +1,27 @@
 const { app, BrowserWindow } = require('electron');
+const { spawn } = require('child_process');
+let serverProcess = null;
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 1000,
     height: 500,
     resizable: false,
-    titleBarStyle: 'hiddenInset',
+    webPreferences: {
+      nodeIntegration: true,
+    },
   });
   win.setMenuBarVisibility(false);
   win.setTitle('Currency');
   win.loadFile('src/index.html');
 };
-app.whenReady().then(() => createWindow());
-app.on('window-all-closed', () => app.quit());
+
+app.whenReady().then(() => {
+  serverProcess = spawn('node', ['server.js'], { detached: true });
+  createWindow();
+});
+
+app.on('window-all-closed', () => {
+  serverProcess.kill();
+  app.quit();
+});
